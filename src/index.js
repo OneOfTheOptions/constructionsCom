75b10bbd741e0a77c84d821fd313288a7d4dbc14
Selectys <selectys.x@gmail.com>
2022-06-10 21:28:14 +0300

    facts start

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './styles/index.scss'
 import './pug/pages/index.pug'
 
 import './js/components/sliders.js'
+import './js/components/progress.js'
 
 // import $ from "jquery";
 // import 'bootstrap';
---
+++ src/js/components/progress.js
@@ -0,0 +1,12 @@
+const circle = document.querySelector('.progress');
+
+const progressAnimation = () => {
+    let percentProgress = Math.floor(85);
+
+    let radius = circle.getAttribute('r');
+    let circleLength = 2*Math.PI * radius;
+    circle.setAttribute('stroke-dasharray', circleLength);
+    circle.setAttribute('stroke-dashoffset', circleLength - circleLength * percentProgress / 100);
+}
+
+progressAnimation();
