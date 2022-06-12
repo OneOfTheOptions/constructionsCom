f605ac8f704ba6aec76272d89f227e09d50dc3da
Selectys <selectys.x@gmail.com>
2022-06-12 13:30:35 +0300

    facts

--- src/js/components/progress.js
+++ src/js/components/progress.js
@@ -1,29 +1,26 @@
 const circles = document.querySelectorAll('.facts-element__circle');
 circles.forEach(el =>{
 
-    if(el.dataset.percentage == 'true') {
+    if(el.dataset.percentage === 'true') {
         let progress = el.querySelector('.progress');
         let valueBlock = el.querySelector('.facts-element__value');
         let radius = progress.getAttribute('r');
         let circleLength = 2 * Math.PI * radius;
-        let full = el.dataset.full;
+        let data = el.dataset.full;
         let value = el.dataset.value;
-        let percentageProgress = Math.floor(value / full * 100);
-        valueBlock.textContent = "value";
-        progress.setAttribute('stroke-dasharray', circleLength);
+        let percentageProgress = Math.floor(value / data * 100);
+        valueBlock.textContent = value;
+        progress.setAttribute('stroke-dasharray', `${circleLength}`);
         progress.setAttribute('stroke-dashoffset', circleLength - circleLength * percentageProgress / 100);
-    }
-    else{
+    } else{
     let progress = el.querySelector('.progress');
-    let value = el.querySelector('.facts-element__value');
+    let valueBlock = el.querySelector('.facts-element__value');
     let radius = progress.getAttribute('r');
     let circleLength = 2 * Math.PI * radius;
-    el.progress('.progress').setAttribute('stroke-dasharray', circleLength);
-
     let percent = el.dataset.percent;
-    let percentageProgress = Math.floor(percent);
-    value.textContent = percent + '%';
+    let percentProgress = Math.floor(parseInt(percent));
+    valueBlock.textContent = percent + '%';
     progress.setAttribute('stroke-dasharray', circleLength);
-    progress.setAttribute('stroke-dashoffset', circleLength - circleLength * percentageProgress / 100);
+    progress.setAttribute('stroke-dashoffset', circleLength - circleLength * percentProgress / 100);
     }
 })
