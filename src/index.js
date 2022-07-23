f0f9b96cbef5b21a5aa688b805fc060c1417cc03
Selectys <selectys.x@gmail.com>
2022-07-23 23:04:41 +0300

    interiorDesign page starting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './styles/index.scss'
 import './pug/pages/index.pug'
 import './pug/pages/services.pug'
 import './pug/pages/interior-design.pug'
+import './styles/index.scss'
 
 import './js/components/sliders.js'
 import './js/components/progress.js'
