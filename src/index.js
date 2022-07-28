d4959b099f7caa62eae5285329111f61b95598a3
Selectys <selectys.x@gmail.com>
2022-07-29 01:50:11 +0300

    we-offer accordion in work

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import './styles/index.scss'
 
 import './js/components/sliders.js'
 import './js/components/progress.js'
-import './js/components/accordion.js'
+import './js/vendors/accordion.js'
+//import './js/vendors/accordion_interior-design.js'
+//import './js/components/accordions.js'
 
 // import $ from "jquery";
 // import 'bootstrap';
---
+++ src/js/components/accordions.js
@@ -0,0 +1,9 @@
+if (document.querySelector('.we-offer')) {
+    const accordion1 = new accordion('.accordion-1', {
+        speed: 300
+    });
+
+    const accordion2 = new accordion('.accordion-2', {
+        speed: 300
+    });
+}
