1e9d9b36a66ac1df9af9fec4852e5d212b0d3243
Selectys <selectys.x@gmail.com>
2022-08-20 23:07:58 +0300

    our work page done

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './styles/index.scss'
 
 import './js/components/sliders.js'
 import './js/components/progress.js'
+import './js/components/portfolio-tabs.js'
 import './js/vendors/accordion.js'
 //import './js/vendors/accordion_interior-design.js'
 //import './js/components/accordions.js'
